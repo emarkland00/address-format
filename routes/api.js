989c4e5cb26dd2d88f3bf0b63e4de22ec92a5128
Errol Markland <ja23jr@gmail.com>
2017-03-21 00:04:47 -0400

    Use express generator to help with generating the front end.

--- routes/api.js
+++ routes/api.js
@@ -9,6 +9,10 @@ function capitalizeISOCode(iso) {
     return iso.toUpperCase();
 }
 
+router.get("/", function(req, res) {
+    res.render('api', { title: 'Address Format API' });
+});
+
 router.get("/format", function (req, res) {
 	var iso = capitalizeISOCode(req.query.iso || '');
 	if (!iso) {
